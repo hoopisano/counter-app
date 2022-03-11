8a761c8a6fa4d06192a6ef1a676643f7274a1367
hoopisano <andrewpisano3@gmail.com>
2022-03-11 12:55:47 -0500

    lets and consts

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let incr = document.querySelector("#incr")
+const incr = document.querySelector("#incr")
 let count = document.querySelector("#count")
-let decr = document.querySelector("#decr")
+const decr = document.querySelector("#decr")
 
 incr.addEventListener("click", () => {
     count.innerHTML = Number(count.innerHTML) + 1;
